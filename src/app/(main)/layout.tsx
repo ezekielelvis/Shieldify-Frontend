fc013a1f49c2de96fc7fec97febd1d13f62bff65
ezekielelvis <ezeamartey777@gmail.com>
2024-07-17 00:48:24 +0000

    Refactor layout components

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,39 +1,35 @@
-'use client'
+"use client"
 
-import { Sidebar } from "@/components/ui/navigation/sidebar";
-import { createContext, useState } from "react";
-interface HomePageProps{
-    children: React.ReactNode
+import { Sidebar } from "@/components/ui/navigation/sidebar"
+import { ThemeProvider } from "next-themes"
+import { createContext, useState } from "react"
+interface HomePageProps {
+  children: React.ReactNode
 }
 
 interface State {
-  tab: number;
-  handleTab: (value: number) => void;
-}
-   
-
-export const Tab = createContext({});
-
-const HomeLayout=({
-    children
-}:HomePageProps)=>{
-    const [tab, setTab] = useState<State["tab"]>(1)
-
-    const handleTab = (value: number) => {
-      setTab(value)
-    }
-    return(
-        <div className="mx-auto max-w-screen-2xl">
-        
-          <Sidebar />
-          <Tab.Provider value={{tab, handleTab}}>
-            <main className="lg:pl-72">{children}</main>
-          </Tab.Provider>
-         
-       
-      </div>
-    )
+  tab: number
+  handleTab: (value: number) => void
 }
 
+export const Tab = createContext({})
 
-export default HomeLayout;
+const HomeLayout = ({ children }: HomePageProps) => {
+  const [tab, setTab] = useState<State["tab"]>(1)
+
+  const handleTab = (value: number) => {
+    setTab(value)
+  }
+  return (
+    <div className="mx-auto max-w-screen-2xl">
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Sidebar />
+        <Tab.Provider value={{ tab, handleTab }}>
+          <main className="lg:pl-72">{children}</main>
+        </Tab.Provider>
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default HomeLayout
